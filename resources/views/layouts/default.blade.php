ebcaf017b8ec4e92ae7e5918a0a7e01ee14cf7b3
ly <kl078101@qq.com>
2021-12-26 08:54:42 +0000

    样式初始化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
-<html>
+<html lang='zh_CN'>
   <head>
     <title>@yield('title', '句子鸭鸭') - 句子鸭鸭</title>
+
+    {{-- 引入 CSS --}}
+    <link rel="stylesheet" type="text/css" href="style/css/css.css" />
+
+    {{-- 引入 Bootstrap CDN--}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class='container'>
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,17 @@
 @section('title', '主页')
 
 @section('content')
-  <h1>主页</h1>
+
+  <div class="jumbotron">
+    <h1>Hello,亲爱的！</h1>
+    <br>
+    <p class="lead">
+      一切，将从这里开始。
+    </p>
+    <br>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
+
 @stop
