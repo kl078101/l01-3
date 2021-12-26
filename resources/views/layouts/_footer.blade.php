256c87d6400b8988eeb94fa7399e47b566451b4a
ly <kl078101@qq.com>
2021-12-26 09:11:45 +0000

    头部和尾部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <a href="https://beian.miit.gov.cn" target=_blank>
+    粤ICP备2021174821号
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -15,19 +15,16 @@
 
   </head>
   <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
+    {{-- 引入头部 --}}
+    @include('layouts._header')
 
     <div class='container'>
       @yield('content')
+
+      {{-- 引入尾部 --}}
+      @include('layouts._footer')
     </div>
+
+
   </body>
 </html>
