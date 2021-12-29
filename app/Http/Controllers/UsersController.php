58fa07f216ff1458fb24601d646ded8462ce65d7
ly <kl078101@qq.com>
2021-12-29 09:36:30 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -41,5 +41,31 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+    // 用户资料编辑
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // 处理编辑后提交的资料
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
+    }
+
 
 }
