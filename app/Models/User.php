ecd401c4204ac8699ecc40f5b7d1bcdb1d484a18
ly <kl078101@qq.com>
2021-12-29 15:06:30 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,10 +46,14 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    // 首页动态流
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 
     // 关注
