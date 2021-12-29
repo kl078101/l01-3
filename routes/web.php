aec7508a1c804109088af47d1b58ad447e0b9b03
ly <kl078101@qq.com>
2021-12-29 12:49:14 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,10 @@ Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
 // 邮件
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+// 重置密码
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
