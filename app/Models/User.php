4504d0899ced733104a27ffd435216676185ad5f
ly <kl078101@qq.com>
2021-12-29 13:33:09 +0000

    数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,22 +13,14 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
@@ -43,12 +35,14 @@ class User extends Authenticatable
         });
     }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // 模型关联
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
